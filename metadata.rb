cbecb7813edf88a90ff57dee5079e604f56c0681
Sam Clements <sam.clements@datasift.com>
2014-01-14 17:12:57 +0000

    Released under the MIT licence

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'search-utils'
 maintainer       'Sam Clements'
 maintainer_email 'sam.clements@borntyping.co.uk'
-license          ''
+license          'mit'
 description      'Wrapper around the partial_search cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 depends          'partial_search'
